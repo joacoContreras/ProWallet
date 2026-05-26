379cea4b36f1553c3ecdfd1b5ee4d2375096582d
Martin Gonzalez <mgoznalez309@alumnos.iua.edu.ar>
2026-05-26 11:24:53 -0300

    fix: eliminar import duplicado de clickable en Components.kt

--- app/src/main/java/com/undef/prowallet/ui/components/Components.kt
+++ app/src/main/java/com/undef/prowallet/ui/components/Components.kt
@@ -3,7 +3,6 @@ package com.undef.prowallet.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
